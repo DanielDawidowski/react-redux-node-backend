e59fc055c44fa2a0c5e357ada4d00e84bcf4290b
Daniel Dawidowski <dvds1987@gmail.com>
2020-10-28 14:58:54 +0100

    Routes Autoloading

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const { readdirSync } = require("fs");
 require("dotenv").config();
-
-// import routes
-const authRoutes = require("./routes/auth");
 
 // app
 const app = express();
@@ -28,14 +26,7 @@ app.use(bodyParser.json({ limit: "2mb" }));
 app.use(cors());
 
 // routes middleware
-app.use("/api", authRoutes);
-
-// route
-app.get("/api", (req, res) => {
-  res.json({
-    data: "hey you hit node API",
-  });
-});
+readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
 
 // port
 const port = process.env.PORT || 8000;
