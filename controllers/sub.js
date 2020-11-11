39a0c846c2ad8c6a38859085598176bcba3acaae
Daniel Dawidowski <dvds1987@gmail.com>
2020-11-11 10:30:05 +0100

    Sub Update Controller Change

--- controllers/sub.js
+++ controllers/sub.js
@@ -6,7 +6,7 @@ exports.create = async (req, res) => {
     const { name, parent } = req.body;
     res.json(await new Sub({ name, parent, slug: slugify(name) }).save());
   } catch (err) {
-    console.log("SUB CREATE ERROR", err);
+    console.log("SUB CREATE ERR ----->", err);
     res.status(400).send("Create sub failed");
   }
 };
@@ -20,11 +20,11 @@ exports.read = async (req, res) => {
 };
 
 exports.update = async (req, res) => {
-  const { name } = req.body;
+  const { name, parent } = req.body;
   try {
     const updated = await Sub.findOneAndUpdate(
       { slug: req.params.slug },
-      { name, slug: slugify(name) },
+      { name, parent, slug: slugify(name) },
       { new: true }
     );
     res.json(updated);
